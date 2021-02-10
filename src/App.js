33a3463368516634605f769ae7ffb9ca13a4660f
ttaerrim <ltr0121@naver.com>
2021-02-10 21:23:36 +0900

    setStatus 실습

--- src/App.js
+++ src/App.js
@@ -57,19 +57,40 @@ const princesses = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      {princesses.map((princess) => (
-        <Disney
-          key={princess.id}
-          name={princess.name}
-          image={princess.image}
-          rating={princess.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+
+  minus = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+
+        {princesses.map((princess) => (
+          <Disney
+            key={princess.id}
+            name={princess.name}
+            image={princess.image}
+            rating={princess.rating}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
